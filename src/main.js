61788674662a76e10f5dbfc6a59417bc05138c3f
Timmy <jushcare@gmail.com>
2020-03-12 18:51:26 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex';
+import Router from './routes.js'
+
+Vue.use(Vuex);
+
+
+Vue.http.options.root = 'http://127.0.0.1:8080'
 
 Vue.config.productionTip = false
 
